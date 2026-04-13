395372395d8e322f16ad5f2f0af46557e209dbfb
webermuros1 <webermuros1@gmail.com>
2026-04-13 13:38:43 +0200

    Initial commit

---
+++ src/test/java/inditex/P1/Gym/GymApplicationTests.java
@@ -0,0 +1,13 @@
+package inditex.P1.Gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
