71b1fcdc1fbf1ac0fb3e8e46d4722011ae8bf372
Adrian-1986 <adryhonor9@gmail.com>
2026-04-14 14:13:09 +0200

    feat(activity): add get activities by user endpoint

---
+++ src/main/java/inditex/P1/Gym/controller/UserActivityController.java
@@ -0,0 +1,27 @@
+package inditex.P1.Gym.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import inditex.P1.Gym.dto.ActivityDTO;
+import inditex.P1.Gym.service.ActivityService;
+
+@RestController
+@RequestMapping("/users")
+public class UserActivityController {
+
+    private final ActivityService activityService;
+
+    public UserActivityController(ActivityService activityService) {
+        this.activityService = activityService;
+    }
+
+    @GetMapping("/{userId}/activities")
+    public List<ActivityDTO> getActivitiesByUser(@PathVariable Long userId) {
+        return activityService.getActivitiesByUser(userId);
+    }
+}
