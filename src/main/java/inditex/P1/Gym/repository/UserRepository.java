1f0e5e17c922a8972e440ca00fd157fc1062e0e9
webermuros1 <webermuros1@gmail.com>
2026-04-14 13:18:53 +0200

    fix:UserRepository

--- src/main/java/inditex/P1/Gym/repository/UserRepository.java
+++ src/main/java/inditex/P1/Gym/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package inditex.P1.Gym.repository;
 
 
-import org.apache.catalina.User;
+import inditex.P1.Gym.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +12,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByDni(String dni);
-    List<User> findActiveTrue();
+    List<User> findByActiveTrue();
 }
